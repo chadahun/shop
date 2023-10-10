cabeee65b74214a310d871e9c99014e4034c7d06
chadahun <cdhqw@naver.com>
2023-10-10 21:33:55 +0900

    1010

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container1 from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import data from './data.js';
+import React, { useEffect, useState } from "react";
+import {Routes,Route,Link} from 'react-router-dom';
 
 function App() {
+  let [shoes] = useState(data)
   return (
     <div className="App">
+      
       <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">Shop</Navbar.Brand>
@@ -37,28 +42,61 @@ function App() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    <div className='main-bg'></div>
-    <Container1>
-      <Row>
-        <Col>
-          <img src='https://codingapple1.github.io/shop/shoes1.jpg' width='80%'></img>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </Col>
-        <Col>
-          <img src='https://codingapple1.github.io/shop/shoes2.jpg' width='80%'></img>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </Col>
-        <Col>
-          <img src='https://codingapple1.github.io/shop/shoes3.jpg' width='80%'></img>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </Col>
-      </Row>
-    </Container1>
+    <Link to="/">홈</Link>
+    <Link to="/detail">상세페이지</Link>
+
+
+    <Routes>
+        <Route path="/" element={
+          <>
+              <div className='main-bg'></div>
+              <Container1>
+                <Row>
+                  {/* <Card shoes={shoes[1]} i={2} ></Card>
+                  <Card shoes={shoes[2]} i={3} ></Card> */}
+                  {
+                    shoes.map(function(a, i){
+                      return(
+                        <Card shoes={shoes[i]} i={i} ></Card>
+                      )
+                    })
+                  }
+                </Row>
+              </Container1>
+              </>
+        }></Route>
+        <Route path="/detail" element={
+          <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            </div>
+            <div className="col-md-6">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button> 
+            </div>
+          </div>
+        </div> 
+        
+
+        }></Route>
+      </Routes>
+
+      
+    
     </div>
   );
+}
+function Card (props){
+  return(
+        <Col>
+          <img src={'https://codingapple1.github.io/shop/shoes'+(props.i+1)+'.jpg'} width='80%'></img>
+          <h4>{props.shoes.title}</h4>
+          <p>{props.shoes.price}</p>
+        </Col>
+  )
 }
 
 export default App;
